948acead98f40da02353ff3f9189832c9305d2b1
Eric D. Schabell <eric@schabell.org>
2016-03-23 14:51:41 -0400

    Initial import.

---
+++ README.md
@@ -0,0 +1,93 @@
+App Dev Cloud with JBoss BRMS Install Demo 
+==========================================
+This demo is to install JBoss BRMS in the Cloud based on leveraging the Red Hat Container Development Kit (CDK) and the
+provided OpenShift Enterprise (OSE) image. It delivers a fully functioning JBoss BRMS containerized on OSE.
+
+
+Install on Red Hat CDK OpenShift Enterprise image
+-------------------------------------------------
+1. First complete the installation and start the OpenShift image supplied in the [cdk-install-demo](https://github.com/eschabell/cdk-install-demo).
+
+2. [Download and unzip this demo.](https://github.com/eschabell/rhcs-brms-install-demo/archive/master.zip)
+
+3. Add products to installs directory.
+
+4. To access CDK OSE installation, make sure your ~/.kube/config provides access, see example in support/kube-config-example file.
+
+5. Install [OpenShift Cient Tools](https://developers.openshift.com/managing-your-applications/client-tools.html) if you have not
+	 done so previously.
+
+6. Login to your CDK OpenShift instance:
+
+    ```
+    $ oc login
+
+    Authentication required for https://10.1.2.2:8443 (openshift)
+    Username: admin
+    Password: admin
+
+    Login successful.
+    ```
+
+7. Setup your new project with following commands:
+
+    ```
+    # create a new project.
+    $ oc new-project rhcs-brms-install-demo
+
+    # setup our new build.
+    $ oc new-build jbossdemocentral/developer:jdk8-uid --name=rhcs-brms-install-demo --binary=true
+
+    # start a build, run from root of rhcs-brms-install-demo project, this takes some time to upload all of the product sources for
+build.
+    $ oc start-build rhcs-brms-install-demo --from-dir=.
+
+    # watch the build by running the following repeatedly until builds completes.
+    $ oc logs rhcs-brms-install-demo-1-build
+
+    # create a new application.
+    $ oc new-app rhcs-brms-install-demo
+
+    # expose business-central application.
+    $ oc expose service rhcs-brms-install-demo --hostname=rhcs-brms-install-demo.10.1.2.2.xip.io
+    ```
+
+8. Login to JBoss BPM Suite to start developing your BPM projects:
+
+    [http://rhcs-brms-install-demo.10.1.2.2.xip.io/business-central](http://rhcs-brms-install-demo.10.1.2.2.xip.io/business-central)
+    ( u:erics / p:jbossbrms1! )
+
+
+Tip & Trick
+-----------
+This is a good way to look at what is being created during all the 'oc' commands above:
+
+    ```
+    $ oc get all
+
+    NAME                        TYPE                                           FROM       LATEST
+    rhcs-brms-install-demo      Docker                                         Binary     1
+
+    NAME                        TYPE                                           FROM             STATUS     STARTED         DURATION
+    rhcs-brms-install-demo-1    Docker                                         Binary@56ed14a   Running    2 minutes ago   2m11s
+    
+    NAME                        DOCKER REPO                                    TAGS                  UPDATED
+    developer                   jbossdemocentral/developer                     1.0,jdk8-uid,latest   10 minutes ago
+    rhcs-brms-install-demo      172.30.211.34:5000/rhcs-brms-install-demo/rhcs-brms-install-demo                         
+
+    NAME                             READY                                     STATUS     RESTARTS   AGE
+    rhcs-brms-install-demo-1-build   1/1                                       Running    0          2m
+
+
+Supporting Articles
+-------------------
+comming soon...
+
+
+Released versions
+-----------------
+See the tagged releases for the following versions of the product:
+
+- v1.0 - JBoss BRMS 6.2.0-BZ-1299002, JBoss EAP 6.4.4 and running on Red Hat CDK using OpenShift Enterprise image. 
+
+![JBoss BRMS](https://github.com/jbossdemocentral/brms-install-demo/blob/master/support/jboss-brms.png?raw=true)
---
+++ init.sh
@@ -0,0 +1,137 @@
+#!/bin/sh 
+DEMO="Install Demo"
+AUTHORS="Andrew Block, Eric D. Schabell"
+PROJECT="git@github.com:jbossdemocentral/brms-install-demo.git"
+PRODUCT="JBoss BRMS"
+JBOSS_HOME=./target/jboss-eap-6.4
+SERVER_DIR=$JBOSS_HOME/standalone/deployments/
+SERVER_CONF=$JBOSS_HOME/standalone/configuration/
+SERVER_BIN=$JBOSS_HOME/bin
+SRC_DIR=./installs
+SUPPORT_DIR=./support
+PRJ_DIR=./projects
+BRMS=jboss-brms-installer-6.2.0.BZ-1299002.jar
+EAP=jboss-eap-6.4.0-installer.jar
+EAP_PATCH=jboss-eap-6.4.4-patch.zip
+VERSION=6.2
+
+# wipe screen.
+clear 
+
+echo
+echo "#################################################################"
+echo "##                                                             ##"   
+echo "##  Setting up the ${DEMO}                                ##"
+echo "##                                                             ##"   
+echo "##                                                             ##"   
+echo "##     ####  ####   #   #   ###    #####                       ##"
+echo "##     #   # #   # # # # # #       #                           ##"
+echo "##     ####  ####  #  #  #  ##     #####                       ##"
+echo "##     #   # # #   #     #    #    #   #                       ##"
+echo "##     ####  #  #  #     # ###     #####                       ##"
+echo "##                                                             ##"   
+echo "##                                                             ##"   
+echo "##  brought to you by,                                         ##"   
+echo "##             ${AUTHORS}                  ##"
+echo "##                                                             ##"   
+echo "##  ${PROJECT}      ##"
+echo "##                                                             ##"   
+echo "#################################################################"
+echo
+
+# make some checks first before proceeding.	
+if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
+	echo Product sources are present...
+	echo
+else
+	echo Need to download $EAP package from the Customer Portal 
+	echo and place it in the $SRC_DIR directory to proceed...
+	echo
+	exit
+fi
+
+if [ -r $SRC_DIR/$EAP_PATCH ] || [ -L $SRC_DIR/$EAP_PATCH ]; then
+	echo Product patches are present...
+	echo
+else
+	echo Need to download $EAP_PATCH package from the Customer Portal 
+	echo and place it in the $SRC_DIR directory to proceed...
+	echo
+	exit
+fi
+
+if [ -r $SRC_DIR/$BRMS ] || [ -L $SRC_DIR/$BRMS ]; then
+		echo Product sources are present...
+		echo
+else
+		echo Need to download $BRMS installer from the Customer Portal 
+		echo and place it in the $SRC_DIR directory to proceed...
+		echo
+		exit
+fi
+
+# Remove the old JBoss instance, if it exists.
+if [ -x $JBOSS_HOME ]; then
+	echo "  - removing existing JBoss product..."
+	echo
+	rm -rf $JBOSS_HOME
+fi
+
+# Run installers.
+echo "JBoss EAP installer running now..."
+echo
+java -jar $SRC_DIR/$EAP $SUPPORT_DIR/installation-eap -variablefile $SUPPORT_DIR/installation-eap.variables
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during JBoss EAP installation!
+	exit
+fi
+
+echo
+echo "Applying JBoss EAP 6.4.4 patch now..."
+echo
+$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during JBoss EAP patching!
+	exit
+fi
+			
+echo
+echo "JBoss BRMS installer running now..."
+echo
+java -jar $SRC_DIR/$BRMS $SUPPORT_DIR/installation-brms -variablefile $SUPPORT_DIR/installation-brms.variables
+
+if [ $? -ne 0 ]; then
+	echo Error occurred during $PRODUCT installation
+	exit
+fi
+
+echo
+echo "  - enabling demo accounts role setup in application-roles.properties file..."
+echo
+cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
+
+echo "  - setting up standalone.xml configuration adjustments..."
+echo
+cp $SUPPORT_DIR/standalone.xml $SERVER_CONF
+
+echo "  - setup email notification users..."
+echo
+cp $SUPPORT_DIR/userinfo.properties $SERVER_DIR/business-central.war/WEB-INF/classes/
+
+# Add execute permissions to the standalone.sh script.
+echo "  - making sure standalone.sh for server is executable..."
+echo
+chmod u+x $JBOSS_HOME/bin/standalone.sh
+
+echo "You can now start the $PRODUCT with $SERVER_BIN/standalone.sh"
+echo
+echo "Login to http://localhost:8080/business-central   (u:erics / p:jbossbrms1!)"
+echo
+
+echo "$PRODUCT $VERSION $DEMO Setup Complete."
+echo
+
